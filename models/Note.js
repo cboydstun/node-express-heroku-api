f4e76f289fa19f8c05c57ce463260d7ca8775b4d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-07-14 10:31:57 -0500

    revised timestamps

--- models/Note.js
+++ models/Note.js
@@ -4,8 +4,6 @@ const Schema = mongoose.Schema;
 const NoteSchema = new Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    created: { type: Date, default: Date.now },
-    updated: { type: Date, default: Date.now },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Note', NoteSchema);
