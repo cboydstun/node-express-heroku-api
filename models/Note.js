fdd3b3297a2fdaee43ffef01e33082be8e63fd10
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-07-14 11:00:28 -0500

    no version key

--- models/Note.js
+++ models/Note.js
@@ -4,6 +4,6 @@ const Schema = mongoose.Schema;
 const NoteSchema = new Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-}, { timestamps: true });
+}, { timestamps: true, versionKey: false });
 
 module.exports = mongoose.model('Note', NoteSchema);
